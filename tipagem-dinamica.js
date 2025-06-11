aae05bb2409a657a1bfddf33bd6c889fc6d0b696
taitwelve <taynascimento15@gmail.com>
2025-06-11 15:13:41 -0300

    feat: add dynamic typing examples in JavaScript

---
+++ tipagem-dinamica.js
@@ -0,0 +1,12 @@
+let exemplo = 10; // exemplo é do tipo número (Number)
+console.log(exemplo); 
+console.log(typeof exemplo); 
+
+exemplo = 'Olá, mundo!'; // agora exemplo é uma string (String)
+console.log(exemplo); 
+console.log(typeof exemplo); 
+
+exemplo = true; // exemplo agora é um booleano (Boolean)
+console.log(exemplo); 
+console.log(typeof exemplo); 
+
