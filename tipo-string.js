ce9fa545b5817a247cb10da852e856ea86e8b157
taitwelve <taynascimento15@gmail.com>
2025-06-11 14:43:21 -0300

    Atividade extra

--- tipo-string.js
+++ tipo-string.js
@@ -21,3 +21,13 @@ console.log(cifrao)
 console.log(aMaiusculo)
 console.log(tique)
 console.log(hiragana)
+
+
+//Atividade Extra
+let valor1 = 0
+let abacate
+let valor2 = "10"
+
+console.log("valor1 é do tipo:", typeof valor1)
+console.log("abacate é do tipo:", typeof abacate)
+console.log("valor2 é do tipo:", typeof valor2)
