356a44b8a931482f93ffd931935fdfa3c8ef13c6
taitwelve <taynascimento15@gmail.com>
2025-06-11 14:25:24 -0300

    feat: implement initial version of average calculation and greeting

---
+++ tipo-number.js
@@ -0,0 +1,11 @@
+const notaPrimeiroBimestre = 9;
+const notaSegundoBimestre = 7.3;
+const notaTerceiroBimestre = 1;
+const notaQuartoBimestre = Number.parseInt('5');
+
+const total = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4;
+
+console.log(`A média é: ${total.toFixed(1)}`);
+
+const nome = "Alice";
+console.log(`Boas vindas,`+ ' ' + nome + "!");
