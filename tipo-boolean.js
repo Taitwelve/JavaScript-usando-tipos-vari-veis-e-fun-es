25877494cbe2633c42037cf608e402ec6079bbe3
taitwelve <taynascimento15@gmail.com>
2025-06-11 14:53:40 -0300

    fix: exemplo de tipo boolean

---
+++ tipo-boolean.js
@@ -0,0 +1,16 @@
+const estudante = "Fernando";
+const aprovado = true;
+
+if (aprovado === true) {
+    console.log(`Parabéns, ${estudante}! Você foi aprovado! Boas festas!`);
+}else {
+    console.log(`Que pena, ${estudante}. Você não foi aprovado. Continue tentando, boas festas!`);
+}
+
+if (estudante === "Fernando") {
+    console.log(`Boas vindas, ${estudante}!`);
+} else {
+    console.log(`Você não é o Fernando, quem é você?`);
+}
+
+ 
