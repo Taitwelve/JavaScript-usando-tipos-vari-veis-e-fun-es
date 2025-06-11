dfd19281d9e9a2f3f7d13f8c99dc54d9f63d1492
taitwelve <taynascimento15@gmail.com>
2025-06-11 15:37:08 -0300

    feat: add example demonstrating null and undefined variable types in JavaScript

---
+++ exercicio-null-undefined.js
@@ -0,0 +1,7 @@
+let valor1;
+let valor2 = null;
+let valor3 = 0;
+
+console.log("valor1 é do tipo:", typeof valor1); 
+console.log("valor2 é do tipo:", typeof valor2); 
+console.log("valor3 é do tipo:", typeof valor3); 
