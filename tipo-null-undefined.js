6014afb993caf0250246aaf455d8f96916eab878
taitwelve <taynascimento15@gmail.com>
2025-06-11 15:07:03 -0300

    feat: add examples of undefined and null variable types

---
+++ tipo-null-undefined.js
@@ -0,0 +1,11 @@
+// Exemplo de variável do tipo undefined
+let nomeEstudante;
+const professora = "Prof. Ana";
+console.log("O nome do estudante é:", nomeEstudante);
+console.log("O tipo de nomeEstudante é:", typeof nomeEstudante);
+console.log("O nome da professora é:", typeof professora);
+
+//Exemplo de variável do tipo null
+//exemplo de NaN
+let telefoneEstudante = null;
+console.log("O telefone do estudante é:", typeof telefoneEstudante);
