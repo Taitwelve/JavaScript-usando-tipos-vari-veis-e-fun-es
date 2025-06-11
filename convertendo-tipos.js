a15e0f074fa6bd799b933af1ff591282caf393ff
taitwelve <taynascimento15@gmail.com>
2025-06-11 15:22:57 -0300

    feat: add examples of number conversion in JavaScript

---
+++ convertendo-tipos.js
@@ -0,0 +1,8 @@
+Number("1"); // retorna o número 1 
+Number("Alura"); // retorna NaN
+Number(undefined); // retorna NaN
+Number(null); // retorna 0
+Number(true); //rertorna 1
+Number(false); // retorna 0
+Number("true"); // retorna NaN
+Number("false"); // retorna NaN
