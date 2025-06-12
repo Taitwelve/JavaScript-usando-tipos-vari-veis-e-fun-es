c35ed6883fca64f3b3ded4d5c8725aa63f6bc786
taitwelve <taynascimento15@gmail.com>
2025-06-12 10:42:34 -0300

    --

--- exercicio-operadores.js
+++ exercicio-operadores.js
@@ -8,3 +8,7 @@ const bonusAnual = 3000;
 
 const resultado = (salarioMensal - despesasFixas - despesasVariaveis) * 12 + (economiasMensais * 12) + bonusAnual;
 console.log(`O resultado financeiro anual é: R$ ${resultado.toFixed(2)}`);
+
+let resultadoCalculo;
+resultadoCalculo = 10 + 5 * 2 / 3 - 7 + 15 * 3 / 5 + 20 - 4 * 2;
+console.log(`O resultado do cálculo é: ${resultadoCalculo.toFixed(2)}`);
