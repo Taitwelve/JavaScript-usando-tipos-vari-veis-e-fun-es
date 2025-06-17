8fabac728e1cdc8f24ad1ef861cbe6fdae2e83f8
taitwelve <taynascimento15@gmail.com>
2025-06-17 18:09:49 -0300

    fix: ensure console output for resultado2 and add additional results for number comparisons

--- exercicio-number-vs-string.js
+++ exercicio-number-vs-string.js
@@ -12,3 +12,14 @@ if (valor1 === valor2) {
 
 console.log('Resultado 1:', resultado1);
 console.log('Resultado 2:', resultado2);
+
+//---------------------------------------------------------------
+
+const numero = 10;
+const resultado3 = (numero > 5 && numero < 15);
+const resultado4 = (numero === 10 || numero > 20);
+const resultado5 = !(numero < 5);
+
+console.log("Resultado 3:", resultado3);
+console.log("Resultado 4:", resultado4);
+console.log("Resultado 5:", resultado5);
