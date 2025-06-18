3e38ea173f0394c980b8179cf2619854b677c5a9
taitwelve <taynascimento15@gmail.com>
2025-06-18 12:17:06 -0300

    feat: add function to display student names with console output

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function exibeNomeEstudande(nome) {
+    console.log("Meu nome é: " + nome);
+}
+exibeNomeEstudande('Taiane');
+exibeNomeEstudande('João');
+exibeNomeEstudande('Maria');
+exibeNomeEstudande('Pedro');
+
+//-------------------------------------------------------------------------------
+
+
