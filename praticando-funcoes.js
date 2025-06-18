6c03fe2806b3812ebf527edf9dd481cd2a1b9eae
taitwelve <taynascimento15@gmail.com>
2025-06-18 12:23:57 -0300

    feat: add simple function to sum two numbers and log the result

---
+++ praticando-funcoes.js
@@ -0,0 +1,9 @@
+// Exemplo de função simples
+function somarDoisNumeros(numA, numB) {
+    return numA + numB;
+}
+somarDoisNumeros(2, 2);
+const resultado = somarDoisNumeros(2, 2);
+console.log(resultado);
+
+//----------------------------------------------------
