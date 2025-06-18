4a66f0775f351ab3679dd3116da32984fbb295cc
taitwelve <taynascimento15@gmail.com>
2025-06-18 12:21:40 -0300

    feat: add function to display student information with formatted output

--- funcoes.js
+++ funcoes.js
@@ -8,4 +8,11 @@ exibeNomeEstudande('Pedro');
 
 //-------------------------------------------------------------------------------
 
+function exibeInfosEstudante(nome, idade, curso, nota) {
+    return(`Nome: ${nome}, Idade: ${idade}, Curso: ${curso} , Nota: ${nota}`);
+}
 
+console.log(exibeInfosEstudante('Taiane', 21, 'JavaScript', 9.5));
+console.log(exibeInfosEstudante('João', 22, 'Python', 8.0));
+console.log(exibeInfosEstudante('Maria', 20, 'Java', 7.5));
+console.log(exibeInfosEstudante('Pedro', 23, 'C++', 6.0));
