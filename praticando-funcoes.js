5fc790f3583faff2afaee76c040c9670ab461fa8
taitwelve <taynascimento15@gmail.com>
2025-06-18 12:25:42 -0300

    feat: add function to divide two numbers and log the result

--- praticando-funcoes.js
+++ praticando-funcoes.js
@@ -7,3 +7,11 @@ const resultado = somarDoisNumeros(2, 2);
 console.log(resultado);
 
 //----------------------------------------------------
+
+// Exemplo de função com parâmetros e retorno
+
+function dividir(dividendo, divisor) {
+    return dividendo / divisor;
+}
+const result = dividir(10, 2);
+console.log(`O resultado é ${result}`); // o resultado é 5
