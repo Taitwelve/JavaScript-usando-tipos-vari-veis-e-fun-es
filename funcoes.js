f573d7e23c551992b592c0492e0bf8d39fd44852
taitwelve <taynascimento15@gmail.com>
2025-06-19 11:34:34 -0300

    feat: add greeting function and demonstrate hoisting behavior

--- funcoes.js
+++ funcoes.js
@@ -16,3 +16,10 @@ console.log(exibeInfosEstudante('Taiane', 21, 'JavaScript', 9.5));
 console.log(exibeInfosEstudante('João', 22, 'Python', 8.0));
 console.log(exibeInfosEstudante('Maria', 20, 'Java', 7.5));
 console.log(exibeInfosEstudante('Pedro', 23, 'C++', 6.0));
+
+// Hoisting é o comportamento do JavaScript de mover declarações de funções para o topo do escopo atual. Isso significa que você pode chamar uma função antes de sua declaração no código, pois o JavaScript já a reconhece.
+// Exemplo de Hoisting: 
+function saudacao() {
+    console.log("Olá, seja bem-vindo!");
+}
+saudacao(); 
