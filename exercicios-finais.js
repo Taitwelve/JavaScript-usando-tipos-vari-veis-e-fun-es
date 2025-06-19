9d0821f8c0b70ff5bc9330770209e8635d08eb9c
taitwelve <taynascimento15@gmail.com>
2025-06-19 11:34:51 -0300

    feat: add product calculation function with multiple examples

--- exercicios-finais.js
+++ exercicios-finais.js
@@ -36,3 +36,19 @@ const textoFatiado = textoOriginal.slice(0, 25);
 console.log(textoMaiusculo);
 console.log(textoMinusculo);
 console.log(textoFatiado);
+
+//Exercício Calcular produto
+
+function calculaProduto(a, b = 2, c = 1) {
+  return a * b * c;
+}
+
+const resultado1 = calculaProduto(3);
+const resultado2 = calculaProduto(2, 4);
+const resultado3 = calculaProduto(1, 2, 3);
+const resultado4 = calculaProduto(2, undefined, 5);
+
+console.log("Resultado 1:", resultado1);
+console.log("Resultado 2:", resultado2);
+console.log("Resultado 3:", resultado3);
+console.log("Resultado 4:", resultado4);
